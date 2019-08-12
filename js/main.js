d40bfe4ec38926e9311a6dab7c49e16b8f5748d1
Tyler Childs <tylerwchilds@gmail.com>
2019-08-11 22:19:13 -0700

    toggles for theming

--- js/main.js
+++ js/main.js
@@ -1,33 +1,91 @@
-const activeClass = '-active';
+const html = document.querySelector('html');
+const body = document.querySelector('body');
 
-function handleBrowserTabs(tabs, tabContents, event) {
-    tabs.forEach(x =>
-        x.classList.remove(activeClass)
-    );
-    tabContents.forEach(x =>
-        x.classList.remove(activeClass)
-    );
+initGridlineToggle();
+initThemeToggles();
+initRhythmToggles();
+updateRhythmBackgroundImage();
+blockForms();
 
-    const tab = event.target;
-    const id = tab.dataset.id;
-    const tabContent = document.getElementById(id);
+function initGridlineToggle() {
+    const target = document.querySelector('.js-grid-toggle');
 
-    tab.classList.add(activeClass);
-    tabContent.classList.add(activeClass);
+    if(localStorage.getItem('grid') === "true"){
+        body.classList.add('hide-gridlines');
+    }
+
+    target.addEventListener('click', function(){
+        localStorage.setItem('grid', 
+            body.classList.toggle('hide-gridlines') // returns true or false
+        );
+        updateRhythmBackgroundImage();
+    });
 }
 
+function initThemeToggles() {
+    const toggles = [...document.querySelectorAll('.js-theme-toggle')];
 
-function bindBrowserTabs() {
-    const tabs = document.querySelectorAll('.example-tab');
-    const tabContents = document.querySelectorAll('.example-tab-content');
-    tabs.forEach(x => 
-        x.addEventListener('click', handleBrowserTabs.bind(null, tabs, tabContents))
-    );
+    const handleThemeToggle = ({target}) => {
+        const selectedTheme = target.dataset.theme;
+        html.dataset.theme = selectedTheme;
+
+        localStorage.setItem('theme', selectedTheme);
+        updateRhythmBackgroundImage();
+    };
+
+    toggles.map(toggle => {
+        toggle.addEventListener('click', handleThemeToggle);
+    });
+
+    if(localStorage.getItem('theme')){
+        html.dataset.theme = localStorage.getItem('theme');
+    }
 }
 
-(function(){
-    // document.querySelectorAll('.js-print').addEventListener('click', window.print);
-    
+function initRhythmToggles() {
+    const html = document.querySelector('html');
+    const toggles = [...document.querySelectorAll('.js-rhythm-toggle')];
 
-    bindBrowserTabs();
-})();
+    const handleRhythmToggle = ({target}) => {
+        const selectedRhythm = target.dataset.rhythm;
+        html.dataset.rhythm = selectedRhythm;
+
+        localStorage.setItem('rhythm', selectedRhythm);
+        updateRhythmBackgroundImage();
+    };
+
+    toggles.map(toggle => {
+        toggle.addEventListener('click', handleRhythmToggle);
+    });
+
+    if(localStorage.getItem('rhythm')){
+        html.dataset.rhythm = localStorage.getItem('rhythm');
+    }
+}
+
+function updateRhythmBackgroundImage() {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext('2d');
+  
+    const rhythm = parseFloat(window.getComputedStyle(body).getPropertyValue('line-height'));
+    const color =  window.getComputedStyle(body).getPropertyValue('--cs-color-gray5');
+    canvas.height = rhythm;
+    console.log(rhythm)
+    canvas.width = rhythm;
+  
+    ctx.fillStyle = 'transparent';
+    ctx.fillRect(0, 0, rhythm, rhythm);
+  
+    ctx.fillStyle = color;
+    ctx.fillRect(0, rhythm - 1, rhythm, 1);
+  
+    return body.style.backgroundImage = `url(${canvas.toDataURL()}`;
+}
+
+function blockForms() {
+    document.querySelectorAll('form').forEach(function(node) {
+        node.addEventListener('submit', function(e){
+            e.preventDefault();
+        }); 
+    });
+}
