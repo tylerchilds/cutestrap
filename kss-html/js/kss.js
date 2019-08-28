986dfd3c91407e85fbddcafbcc1f148895b342c6
Tyler Childs <tylerchilds@users.noreply.github.com>
2019-08-27 22:37:08 -0700

    Cutestrap Two (BETA) :tada: (#48)
    * basic working runtime vertical rhythm
    
    * more rhythmic movements
    
    * rhythm is groooooving
    
    * fixing some one-offs
    
    * steaming forward
    
    * improved core and headings
    
    * 4 spaces (ft. commit i should have made first)
    
    * stylelint and spacing
    
    * correct line endings
    
    * stylelint and somefixes
    
    * successfully pass all stylelint configurations
    
    * cleanup button variable complexity
    
    * clean up variable names to not prefix
    
    * added some basic table styling without overkill
    
    * starting to refactor forms a bit
    
    * extrapolate forms to use only psuedos on labels
    
    * unify block level fields to be a single class
    
    * convert radio and checkbox to inline-field
    
    * field-inline instead of inline-field
    
    * prep for singular field class
    
    * singularity achieved for form fields
    
    * fix form spacing and heights
    
    * streamline focus styles globally
    
    * minor font tweaks
    
    * color palette prep work
    
    * migrate neutral colors to new palette
    
    * color palette migration completed
    
    * neutral color tweaks and line-heights
    
    * prettify those forms finally
    
    * multiline form control labels and no stylelint ignore
    
    * minor tweaks
    
    * margin collapse is our friend
    
    * add wrappers and organize styleguide
    
    * more things
    
    * add legacy versions directory
    
    * fix some issues from github
    
    * block level button
    
    * GPL v3
    
    * some readme cleanup
    
    * unix line endings
    
    * don't break that download link my due

--- kss-html/js/kss.js
+++ kss-html/js/kss.js
@@ -1,14 +1,95 @@
-if(localStorage.getItem('grid') === "true"){
-  $('html').addClass('grid--is_active');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+
+initGridlineToggle();
+initThemeToggles();
+initRhythmToggles();
+updateRhythmBackgroundImage();
+blockForms();
+
+function initGridlineToggle() {
+    const target = document.querySelector('.js-grid-toggle');
+
+    if(localStorage.getItem('grid') === "true"){
+        body.classList.add('hide-gridlines');
+    }
+
+    target.addEventListener('click', function(){
+        localStorage.setItem('grid', 
+            body.classList.toggle('hide-gridlines') // returns true or false
+        );
+        updateRhythmBackgroundImage();
+    });
 }
 
-$(function(){
-  $("form").on('submit', function(e){
-    e.preventDefault();
-  });
-});
+function initThemeToggles() {
+    const toggles = [...document.querySelectorAll('.js-theme-toggle')];
 
-$(document).on('click', '.js-grid-toggle', function(){
-  $('html').toggleClass('grid--is_active');
-  localStorage.setItem('grid', $('html').is('.grid--is_active'));
-});
+    const handleThemeToggle = ({target}) => {
+        const selectedTheme = target.dataset.theme;
+        html.dataset.theme = selectedTheme;
+
+        localStorage.setItem('theme', selectedTheme);
+        updateRhythmBackgroundImage();
+    };
+
+    toggles.map(toggle => {
+        toggle.addEventListener('click', handleThemeToggle);
+    });
+
+    if(localStorage.getItem('theme')){
+        html.dataset.theme = localStorage.getItem('theme');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+function initRhythmToggles() {
+    const html = document.querySelector('html');
+    const toggles = [...document.querySelectorAll('.js-rhythm-toggle')];
+
+    const handleRhythmToggle = ({target}) => {
+        const selectedRhythm = target.dataset.rhythm;
+        html.dataset.rhythm = selectedRhythm;
+
+        localStorage.setItem('rhythm', selectedRhythm);
+        updateRhythmBackgroundImage();
+    };
+
+    toggles.map(toggle => {
+        toggle.addEventListener('click', handleRhythmToggle);
+    });
+
+    if(localStorage.getItem('rhythm')){
+        html.dataset.rhythm = localStorage.getItem('rhythm');
+    } else {
+        localStorage.setItem('rhythm', 'normal');
+    }
+}
+
+function updateRhythmBackgroundImage() {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext('2d');
+  
+    const rhythm = parseFloat(window.getComputedStyle(body).getPropertyValue('line-height'));
+    const color =  window.getComputedStyle(body).getPropertyValue('--color-neutral-tint2');
+    canvas.height = rhythm;
+    console.log(rhythm)
+    canvas.width = rhythm;
+  
+    ctx.fillStyle = 'transparent';
+    ctx.fillRect(0, 0, rhythm, rhythm);
+  
+    ctx.fillStyle = color;
+    ctx.fillRect(0, rhythm - 1, rhythm, 1);
+  
+    return body.style.backgroundImage = `url(${canvas.toDataURL()}`;
+}
+
+function blockForms() {
+    document.querySelectorAll('form').forEach(function(node) {
+        node.addEventListener('submit', function(e){
+            e.preventDefault();
+        }); 
+    });
+}
