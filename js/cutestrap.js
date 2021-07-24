ecd7f642dab9d4daa0ccc70ae73a75f89e1071d7
Tyler Childs <email@tychi.me>
2021-07-24 01:06:39 -0700

    feature: cute-reddit plugin

--- js/cutestrap.js
+++ js/cutestrap.js
@@ -1,3 +1,4 @@
 import './plug/icon-.js';
 import './plug/cute-share.js';
 import './plug/cute-modal.js';
+import './plug/cute-reddit.js';
---
+++ js/plug/cute-reddit.js
@@ -0,0 +1,133 @@
+import tag from '../cutestrap-core.js';
+
+const {
+  render,
+  css,
+  set,
+  get,
+  on
+} = tag('cute-reddit');
+
+render(({ target }) => {
+  const { children = [], after = '' } = query(target);
+
+  const list = children
+    .map(({ data }) => `
+      <li>
+        ${renderPost(data)}
+      </li>
+    `)
+    .join("");
+
+  return `
+    <ol>
+      ${list}
+    </ol>
+    <button class="more" data-after="${after}">
+      Load More
+    </button>
+  `;
+})
+
+css(`
+  {
+    display: block;
+    background: red;
+  }
+`);
+
+function query(target) {
+  const r = target.getAttribute('r') || 'all';
+  const sort = target.getAttribute('sort') || '';
+  const after = target.getAttribute('after') || '';
+
+  const query = { r, sort, after };
+
+  const lookup = JSON.stringify(query);
+
+  const { dataset } = target;
+
+  if(lookup !== dataset.lookup) {
+    target.dataset.lookup = lookup;
+    request(target, query);
+  }
+
+  const state = get();
+  return state[target.id] || {};
+}
+
+async function request(target, query) {
+  const response = await askReddit(query);
+  set(response, middleware(target.id));
+}
+
+async function askReddit({
+  r,
+  sort = '',
+  after = ''
+}) {
+  return await fetch(`https://www.reddit.com/r/${r}/${sort}/.json?count=25&after=${after}`)
+    .then(res => res.json())
+    .then(json => json.data);
+
+}
+
+function middleware(id) {
+  return function (state, payload = {children: []}) {
+    let children = [];
+    if(state[id]) {
+      children = state[id].children;
+    }
+
+    return {
+      ...state,
+      [id]: {
+        ...state[id],
+        after: payload.after,
+        children: [
+          ...children,
+          ...payload.children
+        ]
+      }
+    }
+  }
+}
+
+function renderPost(data) {
+  const renderers = {
+    'image': (data) => `
+      <details>
+        <summary>
+          <a href="${data.url}">${data.title}</a>
+          (<a href="https://old.reddit.com${data.permalink}">Permalink</a>)
+        </summary>
+        <img src="${data.url}" />
+      </details>
+    `,
+    'hosted:video': (data) => `
+      <details>
+        <summary>
+          <a href="${data.url}">${data.title}</a>
+          (<a href="https://old.reddit.com${data.permalink}">Permalink</a>)
+        </summary>
+        <video controls muted="false">
+          <source src="${data.secure_media.reddit_video.fallback_url}" type="video/mp4">
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+      </details>
+    `,
+    'default': (data) => `
+      <a href="${data.url}">${data.title}</a>
+      (<a href="https://old.reddit.com${data.permalink}">Permalink</a>)
+    `
+  }
+
+  return (renderers[data.post_hint] || renderers['default'])(data);
+}
+
+on('click', '.more', function more({ target }) {
+  const { after } = target.dataset;
+  const element = target.closest('cute-reddit');
+  element.setAttribute('after', after);
+  set({ touched: true });
+});
